ff38bc8094bbea95e4141db03941400aeb543860
mpadilhaa <mpadilhaaac@gmail.com>
2023-05-30 16:42:57 -0300

    "alguns ajustes no estilo"

--- src/components/CardMovies/CardMovies.jsx
+++ src/components/CardMovies/CardMovies.jsx
@@ -3,15 +3,16 @@ import * as Styled from "./styles";
 import { Link } from "react-router-dom";
 import imgNoImage from "../../assets/imageError.png";
 
-const CardMovies = ({ movie, showLink = true, overview = false }) => (
+const CardMovies = ({ movie, showLink = true, overview = false, widthImg='w300' }) => (
   <Styled.Container>
     <Styled.Image>
-      {!movie.backdrop_path && <img src={imgNoImage} alt="" />}
-      <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt="" />
+      
+      <img src={`https://image.tmdb.org/t/p/${widthImg}${movie.poster_path}`} alt="" />
     </Styled.Image>
 
     <Styled.Content>
       <h4>{movie.release_date}</h4>
+      {overview && <h3>{movie.title}</h3>}
       {overview && <p>{movie.overview}</p>}
       {showLink && (
         <Link to={`/movie/${movie.id}`} style={Styled.stylesLink}>
